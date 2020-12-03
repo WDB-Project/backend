5213052c633eb3cd5788336b67dad6d506505e3e
gautamvenkatesh <vegautam@gmail.com>
2020-12-02 19:28:18 -0800

    change port

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const router = express.Router()
 
 const app = express()
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 3000
 
 const db = mongoose.connection
 
